8ff6fe187c4b6370009fdbfcdc997979f733b8ea
ombandre <omrajeshbandre15@gmail.com>
2025-11-06 18:55:48 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-white">
+      <div className="text-6xl font-extrabold text-blue-600 text-center">
+        College Management System
+      </div>
+    </div>
+  );
+}
+
+export default App
